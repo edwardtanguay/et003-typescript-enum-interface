ec1f4c228eb474add929ef49a3e9a34f6a6dc626
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-03 11:20:54 +0100

    finished

--- src/pages/PageTechBooks.tsx
+++ src/pages/PageTechBooks.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { AppContext } from '../AppContext';
 import { Helmet } from 'react-helmet';
+import { Language } from '../interfaces';
 
 export const PageTechBooks = () => {
 	const { appTitle, techBooks } = useContext(AppContext);
@@ -11,13 +12,20 @@ export const PageTechBooks = () => {
 				<title>{appTitle} - Tech Books</title>
 			</Helmet>
 			<div className="content">
-				{techBooks.map(techBook => {
+				{techBooks.map((techBook) => {
 					return (
 						<div className="techBook" key={techBook.id}>
 							<div className="title">{techBook.title}</div>
-							<div className="language">The language is: {techBook.language}</div>
+							<div className="language">
+								The language is: {techBook.language}
+							</div>
+							{techBook.language === Language.French && (
+								<div className="extraMessage">
+									You might also like these French books
+								</div>
+							)}
 						</div>
-					)
+					);
 				})}
 			</div>
 		</div>
